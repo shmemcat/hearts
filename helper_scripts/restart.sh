4bad9d279163a55c4a3cb869be96e0b8d77ccf12
shmemcat <emilycatellier@gmail.com>
2023-01-11 18:33:31 -0700

    adjusting awk number

--- helper_scripts/restart.sh
+++ helper_scripts/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the process ID of the script we want to stop
-pid=$(forever list | grep "index.js" | awk '{print $4}')
+pid=$(forever list | grep "index.js" | awk '{print $7}')
 
 # Stop the script
 forever stop $pid
