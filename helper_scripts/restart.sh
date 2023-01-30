592b2669a9b841f84c016642a12ff4d061936507
shmemcat <emilycatellier@gmail.com>
2023-01-30 16:22:16 -0700

    Revert "Auxiliary commit to revert individual files from 4977cf20576a53c483a77ed1df9a793af621e154"
    This reverts commit 1ca9c3a92dfb78eb1bb93410f6639be896593851.

---
+++ helper_scripts/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the process ID of the script we want to stop
+pid=$(forever list | grep "index.js" | awk '{print $7}')
+
+# Stop the script
+forever stop $pid
+
+# Start the server
+forever start index.js
