e086fb3efbb833da6c617a42fa74c8a8bf535598
shmemcat <emilycatellier@gmail.com>
2023-01-31 16:13:13 -0700

    Animating buttons on click!

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
 import { faUser as fasUser } from "@fortawesome/pro-solid-svg-icons";
 import { faUser as farUser } from "@fortawesome/pro-regular-svg-icons";
 import { faMoon as fasMoon } from "@fortawesome/pro-solid-svg-icons";
-import { faSun as fasSun } from "@fortawesome/pro-solid-svg-icons";
+import { faSunBright as fasSunBright } from "@fortawesome/pro-solid-svg-icons";
 import { faMusicNote as fasMusicNote } from "@fortawesome/pro-solid-svg-icons";
 import { faMusicNoteSlash as fasMusicNoteSlash } from "@fortawesome/pro-solid-svg-icons";
 import buttons from "@/styles/buttons.module.css";
@@ -21,13 +23,21 @@ export const UserButton = (props) => {
 
 export const NightModeButton = () => {
    const [state, setState] = React.useState(fasMoon);
+   const [animation, setAnimation] = React.useState(false);
+
+   const onClickHandler = () => {
+      setState(state === fasMoon ? fasSunBright : fasMoon);
+      setAnimation(1);
+   };
 
    return (
       <div>
          <FontAwesomeIcon
-            onClick={() => setState(state === fasMoon ? fasSun : fasMoon)}
+            onClick={() => onClickHandler()}
             className={buttons.icon}
             icon={state}
+            clicked={animation}
+            onAnimationEnd={() => setAnimation(0)}
          />
       </div>
    );
@@ -35,17 +45,21 @@ export const NightModeButton = () => {
 
 export const SoundButton = () => {
    const [state, setState] = React.useState(fasMusicNote);
+   const [animation, setAnimation] = React.useState(false);
+
+   const onClickHandler = () => {
+      setState(state === fasMusicNote ? fasMusicNoteSlash : fasMusicNote);
+      setAnimation(1);
+   };
 
    return (
       <div>
          <FontAwesomeIcon
-            onClick={() =>
-               setState(
-                  state === fasMusicNote ? fasMusicNoteSlash : fasMusicNote
-               )
-            }
+            onClick={() => onClickHandler()}
             className={buttons.icon}
             icon={state}
+            clicked={animation}
+            onAnimationEnd={() => setAnimation(0)}
          />
       </div>
    );
