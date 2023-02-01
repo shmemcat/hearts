cc23b895bd11e9e78be49f505ad1ef68580d6a38
shmemcat <emilycatellier@gmail.com>
2023-02-01 13:43:44 -0700

    rearranging helper script

--- helper_scripts/restart.sh
+++ helper_scripts/restart.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# stop the server
-pm2 stop 0
+# catch up with git changes
+git pull
 
 # catch up with package changes
 npm install
 
 # build the app
 npm run build
+
+# stop the server
+pm2 stop 0
 
 # start the server
 pm2 start npm -- start
