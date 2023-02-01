5e2919abcb44a623867c7911efc07180da9191fb
shmemcat <emilycatellier@gmail.com>
2023-02-01 14:42:28 -0700

    Optimizing font loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/reset_css.css";
 import "@/styles/base_style.css";
+import localFont from "@next/font/local";
 import { ThemeProvider } from "next-themes";
 
 /* Fontawesome */
@@ -9,8 +10,36 @@ config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
    return (
-      <ThemeProvider>
-         <Component {...pageProps} />
-      </ThemeProvider>
+      <>
+         <style jsx global>{`
+            :root {
+               --appFont: ${terminalDosis.style.fontFamily};
+            }
+         `}</style>
+         <ThemeProvider>
+            <Component {...pageProps} />
+         </ThemeProvider>
+      </>
    );
 }
+
+const terminalDosis = localFont({
+   src: [
+      {
+         path: "../public/fonts/TerminalDosis-Regular.ttf",
+         weight: "400",
+      },
+      {
+         path: "../public/fonts/TerminalDosis-SemiBold.ttf",
+         weight: "600",
+      },
+      {
+         path: "../public/fonts/TerminalDosis-Bold.ttf",
+         weight: "700",
+      },
+      {
+         path: "../public/fonts/TerminalDosis-ExtraBold.ttf",
+         weight: "900",
+      },
+   ],
+});
